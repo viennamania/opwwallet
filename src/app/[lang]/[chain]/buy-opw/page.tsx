b916ab52850691fc07d3bd6cec890bd84c3edd28
seunghyonpark <creath.park@gmail.com>
2024-11-22 00:04:41 +0900

    remove commented-out interval fetching logic in buy-opw and buy-usdt pages

--- src/app/[lang]/[chain]/buy-opw/page.tsx
+++ src/app/[lang]/[chain]/buy-opw/page.tsx
@@ -635,14 +635,14 @@ export default function Index({ params }: any) {
 
         address && fetchSellOrders();
 
-        
+        /*
         const interval = setInterval(() => {
             fetchSellOrders();
         }, 10000);
 
 
         return () => clearInterval(interval);
-      
+        */
 
 
 
