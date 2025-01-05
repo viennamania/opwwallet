8bcaf3a27e9b838a4263c0bd49ff1cc7db016fc8
seunghyonpark <creath.park@gmail.com>
2025-01-05 16:10:14 +0900

    refactor: move Providers component to _app.tsx and update layout structure

--- src/app/[lang]/[chain]/layout.tsx
+++ src/app/[lang]/[chain]/layout.tsx
@@ -1,18 +1,23 @@
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 
-import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
+   
           {children}
-      </Providers>
+
       </body>
     </html>
   );
+  
 }
