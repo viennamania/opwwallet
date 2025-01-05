05e49a1accaa5e2a36201e254c2d5b487a42bc51
seunghyonpark <creath.park@gmail.com>
2025-01-05 16:45:14 +0900

    refactor: migrate ThirdwebProvider from _app.tsx to layout.tsx and remove unused _app.tsx file

--- src/app/[lang]/[chain]/layout.tsx
+++ src/app/[lang]/[chain]/layout.tsx
@@ -1,6 +1,9 @@
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
 
+import { ThirdwebProvider } from "thirdweb/react";
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,11 +16,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+
+        <ThirdwebProvider>
    
           {children}
+
+        </ThirdwebProvider>
 
       </body>
     </html>
   );
-  
+
 }
